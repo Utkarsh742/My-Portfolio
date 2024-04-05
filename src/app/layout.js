918b2a35d965662a30435012014524dc63b51e27
Utkarsh Dwivedi <68611209+Utkarsh742@users.noreply.github.com>
2024-04-05 18:59:39 +0530

    Update analytics package to version 1.2.2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StyledComponentsRegistry from "../../lib/registry";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <Analytics />
       </body>
     </html>
   );
